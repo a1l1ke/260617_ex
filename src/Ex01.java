673288323a9c45ca4b828d554485d44981bc8261
a1l1ke <onetenthplease@gmail.com>
2026-06-17 10:44:41 +0900

    docs: add comments explaining comments in Java : docs: 자바 주석에 대한 설명 주석 추가

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,8 +1,15 @@
+/** 표준출력과 기타 등등을 학습함 */
 public class Ex01 {
     // 진입점 main
+    /** 메인 메서드네용 */
     public static void main(String[] args) {
         // sout
         System.out.println("자바 야호!");
+
+        // 주석 : //, /* */, /** */
+        // cmd/ctrl + / -> 한 줄 주석 (블럭 지정 후에 이 명령어 -> 한줄을 여러줄 처럼 씀)
+        // cmd/ctrl + shift + / -> 여러 줄 주석 (묶음)
+        // /** */ => 특정한 클래스, 메서드 등에 붙으면 그걸 지칭했을 때 추가 설명.
 
         // (키워드) (타입) 식별자
         int day = 17; // int 정수 타입
